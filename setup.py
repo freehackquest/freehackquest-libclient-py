36a5f79a9b4448cc3b969d230f39ad79589190d7
Evgenii Sopov <mrseakg@gmail.com>
2019-05-14 00:58:05 +0700

    Updated to 0.2.21

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='libfhqcli',
-    version='0.2.16',
+    version='0.2.21',
+    install_requires=['websocket-client>=0.48.0', 'requests>=2.21.0'],
+    keywords=['ctf', 'fhq', 'jeopardy', 'freehackquest'],
     author='FreeHackQuest Team',
     author_email='freehackquest@gmail.com',
     description='FreeHackQuest Python Client Library for fhq-server',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/freehackquest/libfhqcli-py',
-    packages=setuptools.find_packages(),
+    packages=['libfhqcli'],
     classifiers=(
         'Programming Language :: Python :: 2',
         'License :: OSI Approved :: Apache Software License',
